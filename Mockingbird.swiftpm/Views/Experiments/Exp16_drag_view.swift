33669fa3eb4098639f22f8bef13c4a0e02226ef9
Matt Novoselov <matveynovoselov@gmail.com>
2024-01-18 12:43:05 +0100

    Updates to the drag view func

--- Mockingbird.swiftpm/Views/Experiments/Exp16_drag_view.swift
+++ Mockingbird.swiftpm/Views/Experiments/Exp16_drag_view.swift
@@ -9,48 +9,66 @@ import SwiftUI
 
 struct Exp16_drag_view: View {
     var body: some View {
-        VStack{
-            HStack{
-                VStack{
-                    DraggableCircle()
-                    DraggableCircle()
-                    DraggableCircle()
+        ZStack{
+            Rectangle()
+                .foregroundColor(.green)
+                .frame(width: 100, height: 100)
+            
+            VStack {
+                HStack {
+                    VStack {
+                        DraggableCircle()
+                        DraggableCircle()
+                        DraggableCircle()
+                    }
+                    .padding()
+
+                    Spacer()
                 }
-                .padding()
-                
                 Spacer()
             }
-            Spacer()
         }
     }
 }
 
 struct DraggableCircle: View {
     @State private var circlePosition: CGPoint? // Make circlePosition optional
-    
+
     var body: some View {
         let circleSize: CGFloat = 100
         let initialLocation = CGPoint(x: circleSize / 2, y: circleSize / 2)
-        
-        return Circle()
-            .foregroundColor(.red)
-            .position(circlePosition ?? initialLocation)
-            .frame(width: circleSize, height: circleSize)
-            .gesture(
-                DragGesture()
-                    .onChanged { value in
-                        circlePosition = value.location
-                    }
-                    .onEnded { _ in
-                        withAnimation {
-                            circlePosition = initialLocation
-                        }
-                    }
-            )
-            .background(.blue)
-    }
-}
 
-#Preview {
-    Exp16_drag_view()
+        GeometryReader { geometry in
+            Circle()
+                .foregroundColor(.red)
+//                .background(GeometryReader { geometry in
+//                    Color.clear
+//                        .onChange(of: geometry.frame(in: .global)) { newLocalRect in
+//                            
+//                            
+//                                                    
+//                            if let window = UIApplication.shared.connectedScenes.first as? UIWindowScene {
+//                                let newGlobalRect = geometry.frame(in: .global).applying(window.screen.coordinateSpace as! CGAffineTransform)
+//                                print(newGlobalRect)
+//                            }
+//                            
+//
+//                        }
+//                })
+                .position(circlePosition ?? initialLocation)
+                .gesture(
+                    DragGesture()
+                        .onChanged { value in
+                            circlePosition = value.location
+                        }
+                        .onEnded { _ in
+                            withAnimation {
+                                circlePosition = initialLocation
+                            }
+                        }
+                )
+                .background(.blue)
+        }
+        .frame(width: circleSize, height: circleSize)
+    }
 }
