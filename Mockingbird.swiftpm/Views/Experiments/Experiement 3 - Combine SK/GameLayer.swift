c76767f7cc142d2e3289bf68cd45d72546e48aa9
Matt Novoselov <matveynovoselov@gmail.com>
2024-01-22 19:47:01 +0100

    Particles - play only once + customization

--- Mockingbird.swiftpm/Views/Experiments/Experiement 3 - Combine SK/GameLayer.swift
+++ Mockingbird.swiftpm/Views/Experiments/Experiement 3 - Combine SK/GameLayer.swift
@@ -8,19 +8,19 @@
 import SpriteKit
 
 class GameLayer: SKScene {
-    let trailSmoke: SKEmitterNode = SKEmitterNode(fileNamed: "MyParticle.sks")!
-        
     override func didMove(to view: SKView) {
         self.backgroundColor = .clear
         
         self.size = UIScreen.main.bounds.size
         
         self.scaleMode = .resizeFill
-        
-        self.addChild(trailSmoke)
     }
     
     public func test_call(xpos: Double, ypos: Double) {
+        let trailSmoke: SKEmitterNode = SKEmitterNode(fileNamed: "MyParticle.sks")!
+        
+        self.addChild(trailSmoke)
+        
         trailSmoke.position = CGPoint(x: xpos, y: self.size.height - ypos)
     }
 }
