86185091333f6f7729be0ac4746b8ad4dc5ae404
Matt Novoselov <matveynovoselov@gmail.com>
2024-01-22 20:46:37 +0100

    Upates to the Piano

--- Mockingbird.swiftpm/Views/Experiments/Exp23_piano.swift
+++ Mockingbird.swiftpm/Views/Experiments/Exp23_piano.swift
@@ -9,9 +9,21 @@ import SwiftUI
 
 struct Exp23_piano: View {
     var body: some View {
-        HStack{
-            ForEach(0..<7) { _ in
-                pianoKey()
+        ZStack(alignment: .top){
+            Color(.gray)
+                .ignoresSafeArea()
+            
+            HStack{
+                ForEach(0..<7) { _ in
+                    pianoKey()
+                }
+            }
+            
+            HStack{
+                ForEach(0..<6) { index in
+                    smallPianoKey()
+                        .opacity(index == 2 ? 0 : 1)
+                }
             }
         }
     }
@@ -28,6 +40,7 @@ struct pianoKey: View {
                 .foregroundColor(.yellow)
                 .offset(y: noteOffset)
                 .opacity(noteOpacity)
+                .glow(color: .yellow, radius: 20)
             
             Button(
                 action: {
@@ -49,6 +62,46 @@ struct pianoKey: View {
             ) {
                 Rectangle()
                     .frame(width: 100, height: 400)
+                    .foregroundColor(.white)
+//                    .opacity(0.5)
+            }
+        }
+    }
+}
+
+struct smallPianoKey: View {
+    @State var noteOffset: CGFloat = -150
+    @State var noteOpacity: CGFloat = 1
+    
+    var body: some View {
+        ZStack{
+            Circle()
+                .frame(width: 50, height: 50)
+                .foregroundColor(.yellow)
+                .offset(y: noteOffset)
+                .opacity(noteOpacity)
+                .glow(color: .yellow, radius: 20)
+            
+            Button(
+                action: {
+                    playSound(name: "pop", ext: "mp3")
+                    withAnimation(nil){
+                        noteOffset = -150
+                        noteOpacity = 1
+                    }
+                    withAnimation(Animation.easeOut(duration: 3)){
+                        noteOffset = -400
+                        noteOpacity = 0
+                    } completion: {
+                        withAnimation(nil){
+                            noteOffset = -150
+                            noteOpacity = 1
+                        }
+                    }
+                }
+            ) {
+                Rectangle()
+                    .frame(width: 100, height: 300)
                     .foregroundColor(.black)
 //                    .opacity(0.5)
             }
