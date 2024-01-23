10125342f9f76580cba14a1fe25aced65b6fde52
Matt Novoselov <matveynovoselov@gmail.com>
2024-01-23 18:58:06 +0100

    Updates to the piano

--- Mockingbird.swiftpm/Views/Production/Chapters/Guiding light/PianoScene.swift
+++ Mockingbird.swiftpm/Views/Production/Chapters/Guiding light/PianoScene.swift
@@ -1,6 +1,6 @@
 //
 //  SwiftUIView.swift
-//  
+//
 //
 //  Created by Matt Novoselov on 22/01/24.
 //
@@ -24,47 +24,49 @@ struct PianoScene: View {
 }
 
 struct pianoKey: View {
-    @State var noteOffset: CGFloat = -150
-    @State var noteOpacity: CGFloat = 1
+    
     @State var animationHasEnded: Bool = true
+    @State var amountOfNotes: Int = 0
     
     var body: some View {
         ZStack{
-            Image("SF_note")
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-                .frame(height: 65)
-                .glow(color: Color("MB_main_yellow").opacity(0.6), radius: 30)
-                .offset(y: noteOffset)
-                .opacity(noteOpacity)
+            ForEach(0..<amountOfNotes, id: \.self) { index in
+                Note()
+            }
             
             Button(
                 action: {
                     playSound(name: "pop", ext: "mp3")
                     
-                    if !animationHasEnded{
-                        return
-                    }
-                    animationHasEnded = false
-
-                    withAnimation(Animation.easeOut(duration: 3)){
-                        noteOffset = -400
-                        noteOpacity = 0
-                    } completion: {
-                        withAnimation(nil){
-                            noteOffset = -150
-                            noteOpacity = 1
-                            animationHasEnded = true
-                        }
-                    }
+                    amountOfNotes+=1
                 }
             ) {
                 Rectangle()
                     .frame(width: 100, height: 400)
                     .foregroundColor(.white)
-//                    .opacity(0.5)
             }
         }
+    }
+}
+
+struct Note: View {
+    @State var noteOffset: CGFloat = -150
+    @State var noteOpacity: CGFloat = 1
+    
+    var body: some View {
+        Image("SF_note")
+            .resizable()
+            .aspectRatio(contentMode: .fit)
+            .frame(height: 65)
+            .glow(color: Color("MB_main_yellow").opacity(0.6), radius: 30)
+            .offset(y: noteOffset)
+            .opacity(noteOpacity)
+            .onAppear(){
+                withAnimation(Animation.easeOut(duration: 3)){
+                    noteOffset = -400
+                    noteOpacity = 0
+                }
+            }
     }
 }
 
