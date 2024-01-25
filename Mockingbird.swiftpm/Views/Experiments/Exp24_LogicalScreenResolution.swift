f73073173f5cc73871881a2fbcf2205e6adf218b
Matt Novoselov <matveynovoselov@gmail.com>
2024-01-25 08:49:34 +0100

    Experiment logical screen height

---
+++ Mockingbird.swiftpm/Views/Experiments/Exp24_LogicalScreenResolution.swift
@@ -0,0 +1,29 @@
+import SwiftUI
+
+struct Exp24_LogicalScreenResolution: View {
+    var body: some View {
+        GeometryReader { geometry in
+            VStack {
+                Text("Screen Resolution:")
+                Text(getScreenResolution(from: geometry.size))
+                
+                Rectangle()
+                    .frame(width: 100, height: 100)
+                    .position(CGPoint(x: 1366-50, y: 1024-50))
+            }
+        }
+        .ignoresSafeArea()
+    }
+
+    func getScreenResolution(from size: CGSize) -> String {
+        let width = size.width
+        let height = size.height
+
+        return "\(width) x \(height)"
+    }
+}
+
+#Preview {
+    Exp24_LogicalScreenResolution()
+}
+
