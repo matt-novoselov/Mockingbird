ff590c88f9e0745663e5e6cc496c42bd2277055a
Matt Novoselov <matveynovoselov@gmail.com>
2024-01-31 10:32:01 +0100

    Updated texts in the notification view model

--- Mockingbird.swiftpm/Views/Production/Manager Views/Notifications/Models/NotificationsViewModel.swift
+++ Mockingbird.swiftpm/Views/Production/Manager Views/Notifications/Models/NotificationsViewModel.swift
@@ -9,46 +9,41 @@ import Foundation
 
 class NotificationsViewModel{
     var notifications:[Notification] = [
-        Notification(text: "The brain's reward system is developed to reinforce behaviors necessary for survival, such as eating."), // 0
+        Notification(text: "When you eat, the reward center releases dopamine, providing a jolt of satisfaction."), // 0
         
-        Notification(text: "When you consume food, the reward center in your brain releases dopamine, providing a jolt of satisfaction."), // 1
+        Notification(text: "This encourages you to eat again in the future."), // 1
         
-        Notification(text: "This encourages you to eat again in the future."), // 2
+        Notification(text: "But addictive substances can abuse our reward system."), // 2
         
-        Notification(text: "But addictive substances can abuse our reward system."), // 3
+        Notification(text: "Alcohol stimulates the release of dopamine, tricking your brain into feeling pleasure."), // 3
         
-        Notification(text: "For example, alcohol stimulates the release of dopamine in your body, tricking your brain into feeling pleasure."), // 4
+        Notification(text: "The alcohol itself isn't what makes you feel good and it's not healthy."), // 4
         
-        Notification(text: "The alcohol itself isn't what makes you feel good and it's not healthy."), // 5
+        Notification(text: "This association between drinking alcohol and feeling good can result in addiction."), // 5
         
-        Notification(text: "In the long term, this association between drinking alcohol and feeling good can contribute to the development of addictions."), // 6
+        Notification(text: "Addictive use of social media can closely resemble other substance use disorders."), // 6
         
-        Notification(text: "While social media may appear enjoyable and fun, addictive use of these platforms can closely resemble other substance use disorders."), // 7
+        Notification(text: "Likes, shares, and comments offer an endless amount of immediate rewards, leading to the release of dopamine."), // 7
         
-        Notification(text: "Likes, shares, and comments offer an endless amount of immediate rewards with relatively minimal effort, leading to the release of dopamine."), // 8
+        Notification(text: "Due to our dopamine systems, as we consume more, we actually desire more."), // 8
         
-        Notification(text: "Due to our dopamine systems, as we consume more, we actually desire more."), // 9
+        Notification(text: "When experiencing a gambling win, the brain releases dopamine, creating a desire to gamble more."), // 9
         
-        Notification(text: "Gambling can stimulate the brain's reward system similarly to drugs or alcohol, potentially leading to addiction."), // 10
+        Notification(text: "It’s important to remember that fleeting moments of pleasure are not real happiness."), // 10
         
-        Notification(text: "When experiencing a gambling win, the brain releases dopamine, creating a desire to gamble more for satisfaction."), // 11
+        Notification(text: "Gambling can become an unhealthy obsession, resulting in financial and personal problems."), // 11
         
-        Notification(text: "However, it’s important to remember that fleeting moments of pleasure are not real happiness."), // 12
+        Notification(text: "Addictive drugs produce a pleasurable surge of dopamine up to 10 times higher than a natural reward would."), // 12
         
-        Notification(text: "Gambling can shift from an enjoyable diversion to an unhealthy obsession, resulting in financial and personal problems."), // 13
+        Notification(text: "The higher the peak in dopamine, the further below baseline the dopamine drops afterward, and you feel a lack of pleasure."), // 13
         
-        Notification(text: "All addictive drugs produce a pleasurable surge of dopamine up to 10 times higher than a natural reward would."), // 14
+        Notification(text: "You may find that daily things that used to excite you no longer bring you happiness…"), // 14
         
-        Notification(text: "The higher the peak in dopamine, the further below baseline the dopamine drops afterward, and you feel a lack of pleasure."), // 15
+        Notification(text: "Engage in activities that are meaningful to you – it's the first step towards a brighter future."), // 15
         
-        Notification(text: "You may find that daily things that used to excite you no longer bring you happiness…"), // 16
+        Notification(text: "You are not alone. Your family, relatives, and friends can help you navigate through the storm."), // 16
         
-        Notification(text: "You are not alone; life's journey can be challenging. However, there are people and resources that can help you navigate through the storm."), // 17
-        
-        Notification(text: "Engage in activities that are meaningful to you, such as hobbies – it's the first step towards a brighter future."), // 18
-        
-        Notification(text: "Invest time in fostering connections with the people who matter to you – friends, family, and those who share your values."), // 19
-        
-        Notification(text: "Rather than relying on substances or temporary escapes, try to find happiness in the present moment.") // 20
+        Notification(text: "Rather than relying on substances or temporary escapes, try to find happiness in the present moment.") // 17
+
     ]
 }
