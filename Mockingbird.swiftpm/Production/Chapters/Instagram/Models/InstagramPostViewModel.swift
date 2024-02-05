d4a272fa2eeba3edf69fd74a2252dd465871dc9e
Matt Novoselov <matveynovoselov@gmail.com>
2024-02-05 16:48:50 +0100

    Instagram placeholder posts

--- Mockingbird.swiftpm/Production/Chapters/Instagram/Models/InstagramPostViewModel.swift
+++ Mockingbird.swiftpm/Production/Chapters/Instagram/Models/InstagramPostViewModel.swift
@@ -9,9 +9,9 @@ import Foundation
 
 class InstagramPostViewModel{
     var posts:[InstagramPost] = [
-        InstagramPost(username: "@skateboy2004", postedTimeAgo: "1h ago", image: "PH_grid"),
-        InstagramPost(username: "@purrfect_whiskers", postedTimeAgo: "20m ago", image: "PH_grid"),
-        InstagramPost(username: "@epic_explorer", postedTimeAgo: "5h ago", image: "PH_grid"),
-        InstagramPost(username: "@nom_nom_yummer", postedTimeAgo: "1d ago", image: "PH_grid"),
+        InstagramPost(username: "@skateboy2004", postedTimeAgo: "1h ago", image: "insta_skateboard"),
+        InstagramPost(username: "@purrfect_whiskers", postedTimeAgo: "20m ago", image: "insta_cat"),
+        InstagramPost(username: "@epic_explorer", postedTimeAgo: "5h ago", image: "insta_bridge"),
+        InstagramPost(username: "@nom_nom_yummer", postedTimeAgo: "1d ago", image: "insta_food"),
     ]
 }
