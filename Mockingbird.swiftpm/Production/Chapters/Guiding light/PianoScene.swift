ebfd103ff2c8709f8b6ee55f33568ed53b9ade85
Matt Novoselov <matveynovoselov@gmail.com>
2024-02-08 21:32:31 +0100

    Piano sounds

--- Mockingbird.swiftpm/Production/Chapters/Guiding light/PianoScene.swift
+++ Mockingbird.swiftpm/Production/Chapters/Guiding light/PianoScene.swift
@@ -22,13 +22,13 @@ struct PianoScene: View {
             ZStack(alignment: .top){
                 HStack{
                     ForEach(0..<7) { index in
-                        pianoKey(countTotalNotesEmited: $countTotalNotesEmited, selectedStyle: index%3)
+                        pianoKey(countTotalNotesEmited: $countTotalNotesEmited, selectedIndex: index)
                     }
                 }
                 
                 HStack{
                     ForEach(0..<6) { index in
-                        pianoKeySmall(countTotalNotesEmited: $countTotalNotesEmited, selectedStyle: index%3)
+                        pianoKeySmall(countTotalNotesEmited: $countTotalNotesEmited, selectedIndex: index)
                             .opacity(index==2 ? 0 : 1)
                     }
                 }
@@ -57,8 +57,18 @@ struct pianoKey: View {
     @State var amountOfNotes: Int = 0
     @Binding var countTotalNotesEmited: Int
     
-    var selectedStyle: Int = 0
+    var selectedIndex: Int = 0
+    
     let whiteKeys: [String] = ["piano_white_1","piano_white_2","piano_white_3"]
+    let pianoSounds: [String] = [
+        "piano_white_0",
+        "piano_white_1",
+        "piano_white_2",
+        "piano_white_3",
+        "piano_white_4",
+        "piano_white_5",
+        "piano_white_6"
+        ]
     
     var body: some View {
         ZStack{
@@ -68,13 +78,13 @@ struct pianoKey: View {
             
             Button(
                 action: {
-                    playSound(name: "pop", ext: "mp3")
+                    playSound(name: pianoSounds[selectedIndex], ext: "mp3")
                     
                     countTotalNotesEmited+=1
                     amountOfNotes+=1
                 }
             ) {
-                Image(whiteKeys[selectedStyle])
+                Image(whiteKeys[selectedIndex%3])
                     .interpolation(.high)
                     .resizable()
                     .frame(width: 100, height: 400)
@@ -90,8 +100,16 @@ struct pianoKeySmall: View {
     @State var amountOfNotes: Int = 0
     @Binding var countTotalNotesEmited: Int
     
-    var selectedStyle: Int = 0
+    var selectedIndex: Int = 0
     let blackKeys: [String] = ["piano_black_1","piano_black_2","piano_black_3"]
+    let pianoSounds: [String] = [
+        "piano_black_0",
+        "piano_black_1",
+        "piano_black_2",
+        "piano_black_3",
+        "piano_black_4",
+        "piano_black_4",
+        ]
     
     var body: some View {
         ZStack{
@@ -102,13 +120,13 @@ struct pianoKeySmall: View {
             
             Button(
                 action: {
-                    playSound(name: "pop", ext: "mp3")
+                    playSound(name: pianoSounds[selectedIndex], ext: "mp3")
                     
                     countTotalNotesEmited+=1
                     amountOfNotes+=1
                 }
             ) {
-                Image(blackKeys[selectedStyle])
+                Image(blackKeys[selectedIndex%3])
                     .interpolation(.high)
                     .resizable()
                     .frame(width: 80, height: 200)
